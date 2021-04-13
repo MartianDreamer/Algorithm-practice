1d032b984631c25da635f2102fa83400f41d6b0b
Sang Nguyen <nguyenxuansang9494@gmail.com>
2021-04-13 22:58:10 +0700

    sum sub array with empty arr

---
+++ sumsubarraywithemptyarr.py
@@ -0,0 +1,23 @@
+def max_sum(number1, number2):
+    total = number1 + number2
+    if total >= number1 and total >= number2:
+        return total
+    elif number1 >= number2:
+        return number1
+    else:
+        return number2
+
+
+def sum_sub_array(number_list):
+    if len(number_list) == 1:
+        return number_list[0] if number_list[0] >= 0 else 0
+    mid = len(number_list)//2
+    left = number_list[:mid]
+    right = number_list[mid:]
+    sum_left = sum_sub_array(left)
+    sum_right = sum_sub_array(right)
+    result = max_sum(sum_left, sum_right)
+    return result
+
+
+print(sum_sub_array([-2, -13, -9, -6, -7, -3, -5, -15]))
