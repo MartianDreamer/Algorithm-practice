848ca852bb4723fec21c914a3a984a334acf7c85
Sang Nguyen <nguyenxuansang9494@gmail.com>
2021-04-26 18:12:44 +0700

    update heap to implement priority queue

--- Heap.py
+++ Heap.py
@@ -9,16 +9,7 @@ class Heap:
         self.__arrSize = len(arr)
         self.buildHeap()
 
-    def parentOf(self, i):
-        return i//2
-
-    def rightOf(self, i):
-        return i*2+2
-
-    def leftOf(self, i):
-        return i*2+1
-
-    def heapify(self, i: int):
+    def __heapify(self, i: int):
         largest = i
         left = i*2 + 1
         right = i*2 + 2
@@ -30,12 +21,12 @@ class Heap:
             temp = self.__arr[i]
             self.__arr[i] = self.__arr[largest]
             self.__arr[largest] = temp
-            self.heapify(largest)
+            self.__heapify(largest)
 
     def buildHeap(self):
         lastIndex = len(self.__arr)//2
         for i in range(lastIndex + 1):
-            self.heapify(lastIndex - i)
+            self.__heapify(lastIndex - i)
         return self
 
     def heapSort(self) -> List[int]:
@@ -45,15 +36,22 @@ class Heap:
             self.__arr[lastIndex - i] = self.__arr[0]
             self.__arr[0] = temp
             self.__heapSize -= 1
-            self.heapify(0)
+            self.__heapify(0)
         self.__heapSize = self.__arrSize
         return self.__arr
 
+    def maxHeap(self) -> int:
+        return self.__arr[0]
+
+    def extractMax(self) -> int:
+        if self.__heapSize < 1:
+            raise Exception("No element in heap")
+        maxElement = self.__arr[0]
+        self.__arr[0] = self.__arr[self.__heapSize - 1]
+        self.__arr[self.__heapSize - 1] = maxElement
+        self.__heapSize -= 1
+        self.__heapify(0)
+        return maxElement
+
     def __str__(self) -> str:
         return self.__arr.__str__()
-
-
-heap = Heap([5, 13, 2, 25, 7, 17, 20, 8, 4])
-arr = heap.heapSort()
-print(arr)
-print(heap.buildHeap())
