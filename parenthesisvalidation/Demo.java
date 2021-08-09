7193158215f8c429e2aa9135780aec90a1894287
Sang Nguyen Xuan <nguyenxuansang9494@gmail.com>
2021-08-09 23:44:39 +0700

    change demo

--- parenthesisvalidation/Demo.java
+++ parenthesisvalidation/Demo.java
@@ -3,7 +3,7 @@ package parenthesisvalidation;
 public class Demo {
     public static void main(String[] args) {
         var checker = new ParenthesisValidation();
-        if (checker.isValid("(sang(truc(thuong)haha)sa)")) {
+        if (checker.isValid("(()(()()))")) {
             System.out.println("valid");
             return;
         }
